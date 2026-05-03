13016e24c5e7a64bda6a7802b27d16a9cc32cff6
ShiganLuo <2530320102@qq.com>
2026-05-03 12:22:21 +0800

    feat:增加重命名

--- modules/track/bin/track.py
+++ modules/track/bin/track.py
@@ -1,14 +1,16 @@
-from typing import List
+from typing import List, Dict
 import argparse
 import logging
 import glob
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 def ucsc_track_format(
     bigwig_files:List[str],
-    output:str
+    output:str,
+    sra_name_dict:Dict[str, str] = None
 ):
     """
     Convert a list of bigwig files to UCSC track format.
@@ -21,7 +23,11 @@ def ucsc_track_format(
     """
     track_lines = []
     for bigwig_file in bigwig_files:
-        track_name = bigwig_file.split('/')[-1].replace('.bigwig', '')
+        sra_id = bigwig_file.split('/')[-1].replace('.bigwig', '')
+        if sra_name_dict:
+            track_name = sra_name_dict.get(sra_id, sra_id)
+        else:
+            track_name = sra_id
         track_line = f'track type=bigWig name="{track_name}" description="{track_name}" bigDataUrl={bigwig_file}'
         track_lines.append(track_line)
     with open(output, 'w') as f:
@@ -29,9 +35,22 @@ def ucsc_track_format(
     return track_lines
 
 def test_ucsc_track_format():
+    GSM_dict = {}
+    with open("/data/pub/zhousha/20260422_ClIPseq/data/meta/meta.tsv", 'r') as f:
+        for line in f:
+            items = line.strip().split('\t')
+            GSM_dict[items[0]] = items[1]
+    sra_dict = {}
+    df = pd.read_csv("/data/pub/zhousha/20260422_ClIPseq/data/meta/sra_metadata.csv")
+    sra_dict = dict(zip(df['Sample_id'], df['Data_id']))
+    sra_name_dict = {}
+    for k, v in sra_dict.items():
+        name = GSM_dict.get(k, k)
+        sra_name_dict[v] = name
+    
     bigwig_files = glob.glob('/data/pub/zhousha/20260422_ClIPseq/output/CLIP/igv/*.bigwig')
     output = "/data/pub/zhousha/20260422_ClIPseq/output/CLIP/igv/ucsc_tracks.txt"
-    ucsc_track_format(bigwig_files, output)
+    ucsc_track_format(bigwig_files, output, sra_name_dict)
 
 def main():
     parser = argparse.ArgumentParser(description='Convert bigwig files to UCSC track format.')
